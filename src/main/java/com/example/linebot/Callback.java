a7ee388ce83b7357ed7a984096202354bb065b39
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2018-06-02 07:54:53 +0900

    おみくじ処理を追加し、クラスの内容や軽微な問題を修正

--- src/main/java/com/example/linebot/Callback.java
+++ src/main/java/com/example/linebot/Callback.java
@@ -5,6 +5,8 @@ import com.linecorp.bot.model.event.FollowEvent;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.PostbackEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.ImageMessage;
+import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
@@ -16,11 +18,12 @@ import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.time.LocalTime;
+import java.util.Random;
 
 @LineMessageHandler
 public class Callback {
 
-  private static final Logger log = LoggerFactory.getLogger(Push.class);
+  private static final Logger log = LoggerFactory.getLogger(Callback.class);
 
   // マッピングされていないEventに対応する
   @EventMapping
@@ -28,6 +31,7 @@ public class Callback {
     System.out.println("event: " + event);
   }
 
+  // フォローイベントに対応する
   @EventMapping
   public TextMessage handleFollow(FollowEvent event) {
     // 実際はこのタイミングでフォロワーのユーザIDをデータベースにに格納しておくなど
@@ -35,24 +39,27 @@ public class Callback {
     return reply("あなたのユーザIDは " + userId);
   }
 
-  // TextMessageに対応する
+  // 返答メッセージを作る
+  private TextMessage reply(String text) {
+    return new TextMessage(text);
+  }
+
+
+  // 文章で話しかけられたとき（テキストメッセージのイベント）に対応する
   @EventMapping
-  public TextMessage handleMessage(MessageEvent<TextMessageContent> event) {
+  public Message handleMessage(MessageEvent<TextMessageContent> event) {
     TextMessageContent tmc = event.getMessage();
     String text = tmc.getText();
     switch (text) {
       case "やあ":
         return greet();
+      case "おみくじ":
+        return replyOmikuji();
       case "部屋":
         return replyRoomInfo();
       default:
         return reply(text);
     }
-  }
-
-  // 返答メッセージを作る
-  private TextMessage reply(String text) {
-    return new TextMessage(text);
   }
 
   // あいさつする
@@ -68,6 +75,26 @@ public class Callback {
     return reply("おはよう！");
   }
 
+  // 画像メッセージを作る
+  private ImageMessage replyImage(String url) {
+    // 本来は、第一引数が実際の画像URL、第二画像がサムネイルのurl
+    return new ImageMessage(url, url);
+  }
+
+  // ランダムにおみくじ画像を返す
+  private ImageMessage replyOmikuji() {
+    int ranNum = new Random().nextInt(3);
+    switch (ranNum) {
+      case 2:
+        return replyImage("https://3.bp.blogspot.com/-vQSPQf-ytsc/T3K7QM3qaQI/AAAAAAAAE-s/6SB2q7ltxwg/s1600/omikuji_daikichi.png");
+      case 1:
+        return replyImage("https://2.bp.blogspot.com/-27IG0CNV-ZE/VKYfn_1-ycI/AAAAAAAAqXw/fr6Y72lOP9s/s400/omikuji_kichi.png");
+      case 0:
+      default:
+        return replyImage("https://4.bp.blogspot.com/-qCfF4H7YOvE/T3K7R5ZjQVI/AAAAAAAAE-4/Hd1u2tzMG3Q/s1600/omikuji_kyou.png");
+    }
+  }
+
   // センサーの値をWebから取得して、CO2クラスのインスタンスにいれる(******の所は、別途指示します）
   private TextMessage replyRoomInfo() {
     String key = "******";
@@ -76,7 +103,11 @@ public class Callback {
     RestTemplate restTemplate = new RestTemplateBuilder().build();
     try {
       CO2 co2 = restTemplate.getForObject(uri, CO2.class);
-      return reply("二酸化炭素は" + co2.getConcentration() + "ppm、温度は" + co2.getTemperature() + "度です");
+      return reply("二酸化炭素は"
+        + co2.getConcentration()
+        + "ppm、温度は"
+        + co2.getTemperature()
+        + "度です");
     } catch (HttpClientErrorException e) {
       e.printStackTrace();
       return reply("センサーに接続できていません");
