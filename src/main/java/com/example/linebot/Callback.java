4fc06eef69ebbbba05461bd90db114015b4a876c
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2021-05-10 15:38:06 +0900

    Optimize Import

--- src/main/java/com/example/linebot/Callback.java
+++ src/main/java/com/example/linebot/Callback.java
@@ -2,22 +2,15 @@ package com.example.linebot;
 
 import com.example.linebot.replier.*;
 import com.example.linebot.service.RemainderService;
-import com.linecorp.bot.client.LineBlobClient;
-import com.linecorp.bot.client.MessageContentResponse;
-import com.linecorp.bot.model.event.*;
-import com.linecorp.bot.model.event.message.ImageMessageContent;
+import com.linecorp.bot.model.event.FollowEvent;
+import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
-import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.io.IOException;
-import java.util.Optional;
-import java.util.concurrent.ExecutionException;
 
 
 @LineMessageHandler
