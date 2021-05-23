7af7a6895d132dbb3881ce72d2fb00d82b966f0d
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2021-05-24 00:18:46 +0900

    リファクタリング

--- src/main/java/com/example/linebot/replier/CovidReport.java
+++ src/main/java/com/example/linebot/replier/CovidReport.java
@@ -5,12 +5,12 @@ import com.example.linebot.value.CovidItems;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 
+import java.time.Month;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class CovidReport implements Replier {
 
-  private static final String MESSAGE_FORMAT = "%s の %s の新規感染者数は %d 人";
+  private static final String MESSAGE_FORMAT = "%s の %s月%s日 の新規感染者数は %d 人";
 
   private final CovidItems items;
 
@@ -20,32 +20,29 @@ public class CovidReport implements Replier {
 
   @Override
   public Message reply() {
-    List<CovidItem> twoItems = cutOff();
-    String body = getLastCovidInfomation(twoItems);
+    String body = getLastCovidInfomation();
     return new TextMessage(body);
   }
 
-  private String getLastCovidInfomation(List<CovidItem> covidItems) {
-    switch (covidItems.size()) {
-      case 2:
-        // データ取得2日目以降
-        CovidItem before = covidItems.get(0);
-        CovidItem beforeLast = covidItems.get(1);
-        int differ = before.getNpatients() - beforeLast.getNpatients();
-        return String.format(MESSAGE_FORMAT, before.getNameJP(), before.getDate(), differ);
-      case 1:
-        // データ取得初日
-        CovidItem one = covidItems.get(0);
-        return String.format(MESSAGE_FORMAT, one.getNameJP(), one.getDate(), one.getNpatients());
-      default:
-        return "データが不足しています";
+  private String getLastCovidInfomation() {
+    List<CovidItem> list = items.getItemList();
+    if (list.size() >= 2) {
+      // 政府のデータ取得2日目以降
+      CovidItem before = list.get(0);
+      CovidItem beforeLast = list.get(1);
+      int differ = before.getNpatients() - beforeLast.getNpatients();
+      Month month = before.getDate().getMonth();
+      int dayOfMonth = before.getDate().getDayOfMonth();
+      return String.format(MESSAGE_FORMAT, before.getNameJP(), month.getValue(), dayOfMonth, differ);
     }
+    if (list.size() == 1) {
+      // 政府のデータ取得初日
+      CovidItem before = list.get(0);
+      Month month = before.getDate().getMonth();
+      int dayOfMonth = before.getDate().getDayOfMonth();
+      return String.format(MESSAGE_FORMAT, before.getNameJP(), month.getValue(), dayOfMonth, before.getNpatients());
+    }
+    return "データがありません";
   }
 
-  // 不要なデータを切り捨てる
-  private List<CovidItem> cutOff() {
-    return items.getItemList().stream()
-      .limit(2)
-      .collect(Collectors.toList());
-  }
 }
