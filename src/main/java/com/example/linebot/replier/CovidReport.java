6b2000c93def373532d24d17ca02c061592cc682
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2021-05-24 01:07:32 +0900

    メッセージの作成をメソッド分け

--- src/main/java/com/example/linebot/replier/CovidReport.java
+++ src/main/java/com/example/linebot/replier/CovidReport.java
@@ -20,29 +20,33 @@ public class CovidReport implements Replier {
 
   @Override
   public Message reply() {
-    String body = getLastCovidInfomation();
+    String body = "データがありません";
+    List<CovidItem> list = items.getItemList();
+    if (list.size() >= 2) {
+      body = getMessageOf2days();
+    }
+    if (list.size() == 1) {
+      body = getMessageOfLast();
+    }
     return new TextMessage(body);
   }
 
-  private String getLastCovidInfomation() {
+  private String getMessageOf2days() {
     List<CovidItem> list = items.getItemList();
-    if (list.size() >= 2) {
-      // 政府のデータ取得2日目以降
-      CovidItem before = list.get(0);
-      CovidItem beforeLast = list.get(1);
-      int differ = before.getNpatients() - beforeLast.getNpatients();
-      Month month = before.getDate().getMonth();
-      int dayOfMonth = before.getDate().getDayOfMonth();
-      return String.format(MESSAGE_FORMAT, before.getNameJP(), month.getValue(), dayOfMonth, differ);
-    }
-    if (list.size() == 1) {
-      // 政府のデータ取得初日
-      CovidItem before = list.get(0);
-      Month month = before.getDate().getMonth();
-      int dayOfMonth = before.getDate().getDayOfMonth();
-      return String.format(MESSAGE_FORMAT, before.getNameJP(), month.getValue(), dayOfMonth, before.getNpatients());
-    }
-    return "データがありません";
+    CovidItem before = list.get(0);
+    CovidItem beforeLast = list.get(1);
+    int differ = before.getNpatients() - beforeLast.getNpatients();
+    Month month = before.getDate().getMonth();
+    int dayOfMonth = before.getDate().getDayOfMonth();
+    return String.format(MESSAGE_FORMAT, before.getNameJP(), month.getValue(), dayOfMonth, differ);
+  }
+
+  private String getMessageOfLast() {
+    List<CovidItem> list = items.getItemList();
+    CovidItem before = list.get(0);
+    Month month = before.getDate().getMonth();
+    int dayOfMonth = before.getDate().getDayOfMonth();
+    return String.format(MESSAGE_FORMAT, before.getNameJP(), month.getValue(), dayOfMonth, before.getNpatients());
   }
 
 }
